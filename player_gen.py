72733c70987174884f7974d5ef18d025b4f9e62b
amphibiousbean <110147825+amphibiousbean@users.noreply.github.com>
2025-06-02 15:55:18 -0400

    batter generation mostly completed

--- player_gen.py
+++ player_gen.py
@@ -11,8 +11,6 @@ import json
 
 
 ZONE_WIDTH = 0.83
-ZONE_TOP = 3.5
-ZONE_BOTTOM = 1.5
 
 FILEPATH="players.json"
 
@@ -37,7 +35,13 @@ std_dict={
     "bb" : 3.6,
     "k" : 5.9,
     "whiff" : 6.2,
-    "chase" : 6.2
+    "chase" : 6.2,
+    "sweet_spot" : 4.4,
+    "solid_con" : 2.2,
+    "barrel_rate" : 4.9,
+    "hard_hit" : 8.5,
+    "ev50" : 2.6,
+    "z_whiff" : 5.3
 }
 
 avg_bat = {
@@ -47,12 +51,19 @@ avg_bat = {
     "bb" : 8.4,
     "k" : 22.2,
     "whiff" : 23.1,
-    "chase" : 27.5
+    "chase" : 27.5,
+    "sweet_spot" : 35.1,
+    "solid_con" : 6.7,
+    "barrel_rate" : 9.9,
+    "hard_hit" : 43.5,
+    "ev50" : 100.9,
+    "z_whiff" : 16.1
+
 }
 
 
-def calc_att(x,y):
-    att=round(50+(x+y))
+def calc_att(atts):
+    att=round(50+(sum(atts)))
     return max(20, min(80, att))
 
 def calc_stat_impact(x, stat, invert=False):
@@ -74,8 +85,8 @@ while mode != "b" or "p":
     if mode == "b" or "p":
         break
 name=input("Enter player name : ")
-player=tuple(name.lower().split(' '))
-playerid=playerid_lookup(player[1], player[0]).iloc[0]["key_mlbam"]
+player_name=tuple(name.lower().split(' '))
+#playerid=playerid_lookup(player[1], player[0]).iloc[0]["key_mlbam"]
 if mode == "b":
     
     player={
@@ -83,24 +94,45 @@ if mode == "b":
         "name" : name
         }
     #setting dataframes
-    data=batter.pull_player_data(player)
-    batted_balls=batter.filter_non_events(data)
+    data=batter.pull_player_data(player_name)
+    events=batter.filter_non_events(data)
+    xba_filter=batter.filter_for_xba(events)
     at_bats=batter.filter_non_AB(data)
+    batted_balls=batter.only_batted_balls(data)
+
+    #setting batter's strike zone
+    top_zone=data['sz_top'].agg('mean')
+    bot_zone=data['sz_bot'].agg('mean')
+    sz=[top_zone, bot_zone]
 
     #getting stat impact
-    exit_vel=calc_stat_impact(batter.get_exit_vel(batted_balls), "exit_vel")
-    xwoba=calc_stat_impact(batter.get_xwoba(batted_balls), "xwoba")
-    xba=calc_stat_impact(batter.get_xba(batted_balls), "xba")
-    bb=calc_stat_impact(batter.get_bb(data), "bb")
-    k=calc_stat_impact(batter.get_k(data),"k", invert=True)
+    exit_vel=calc_stat_impact(batter.get_exit_vel(events), "exit_vel")
+    xwoba=calc_stat_impact(batter.get_xwoba(events), "xwoba")
+    xba=calc_stat_impact(batter.get_xba(xba_filter), "xba")
+    bb=calc_stat_impact(batter.get_bb(events), "bb")
+    k=calc_stat_impact(batter.get_k(events),"k", invert=True)
     whiff=calc_stat_impact(batter.get_whiff(data),"whiff", invert=True)
-    chase=calc_stat_impact(batter.get_chase(data),"chase",invert=True)
-
-    player["con"] = calc_att(xba,whiff)
-    player["pow"] = calc_att(xwoba, exit_vel)
-    player["vis"] = calc_att(k,whiff)
-    player["disc"] = calc_att(bb, chase)
+    chase=calc_stat_impact(batter.get_chase(data,sz),"chase",invert=True)
+    sweet_spot=calc_stat_impact(batter.get_la_ss_rate(batted_balls), "sweet_spot")
+    solid_con=calc_stat_impact(batter.get_solid_con_rate(batted_balls), "solid_con")
+    barrel_rate=calc_stat_impact(batter.get_barrel_rate(batted_balls), "barrel_rate")
+    hard_hit=calc_stat_impact(batter.get_hard_hit(batted_balls), "hard_hit")
+    ev50=calc_stat_impact(batter.get_ev50(batted_balls), "ev50")
+    z_whiff=calc_stat_impact(batter.get_zone_whiff(data,sz),"z_whiff", invert=True)
     
+    #list to pass to calc_att
+    con=[xba*0.3, solid_con*0.3, sweet_spot*0.3,exit_vel*0.1]
+    pow=[xwoba*0.2,barrel_rate*0.1, hard_hit*0.4, ev50*0.3]
+    vis=[z_whiff*0.25, bb*0.25, whiff*0.5]
+    disc=[chase*0.5, k*0.2, bb*0.3]    
+
+    #calculating attributes
+    player["con"] = calc_att(con)
+    player["pow"] = calc_att(pow)
+    player["vis"] = calc_att(vis)
+    player["disc"] = calc_att(disc)
+    
+    #adding to players.json, printing to cmd
     add_player(player)
     print(player)
     
